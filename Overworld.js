640797df3fa590887ecb1fd9a9da2e6d4c39917c
Adam <algolec1990@yahoo.com>
2023-11-18 11:24:22 -0500

    modify Overworld.js to remove hardcoding.

--- Overworld.js
+++ Overworld.js
@@ -3,25 +3,19 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector('.game-canvas');
     this.ctx = this.canvas.getContext('2d');
+    this.map = null;
+  }
+  startGameLoop() {
+    const step = () => {
+      requestAnimationFrame(() => {
+        step();
+      });
+    };
+    step();
   }
 
   init() {
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0); //
-    };
-    image.src = './images/maps/DemoLower.png';
-
-    //Place game objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    });
-
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: '/images/characters/people/npc1.png',
-    });
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+    this.startGameLoop();
   }
 }
